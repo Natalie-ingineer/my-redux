823c6b84a12f0fb9a571b81b637adc33d530121f
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-02-17 15:19:02 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,52 +1,20 @@
-// import { ContactForm } from "./ContactForm/ContactForm";
-import { ContactForm } from "./ContactForm/ContactForm";
-import { ContactList } from "./ContactList/ContactList";
-import { SearchBox } from "./SearchBox/SearchBox";
-
-import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { deposit, withdraw } from "../redux/balanceSlice";
+import LangSwitcher from "./LangSwitcher";
 
 export const App = () => {
-  const [nameFilter, setNameFilter] = useState("");
-  const [users, setUsers] = useState(() => {
-    const savedUsers = window.localStorage.getItem("users-phonebook");
-
-    if (savedUsers !== null) {
-      return JSON.parse(savedUsers);
-    }
-    return [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ];
-  });
-
-  const visibleUsers = users.filter((user) =>
-    user.name.toLowerCase().includes(nameFilter.toLowerCase())
-  );
-
-  const deleteUser = (userId) => {
-    setUsers((prevUsers) => {
-      return prevUsers.filter((user) => user.id !== userId);
-    });
-  };
-
-  const addUser = (newUser) => {
-    setUsers((prevUsers) => {
-      return [...prevUsers, newUser];
-    });
-  };
-
-  useEffect(() => {
-    window.localStorage.setItem("users-phonebook", JSON.stringify([...users]));
-  }, [users]);
+  const dispatch = useDispatch();
+  const balance = useSelector((state) => state.balance.value);
+  const lang = useSelector((state) => state.locale.lang);
 
   return (
-    <>
-      <h1 className="caption">Phonebook</h1>
-      <ContactForm onAdd={addUser} />
-      <SearchBox value={nameFilter} onFilter={setNameFilter} />
-      <ContactList items={visibleUsers} onDelete={deleteUser} />
-    </>
+    <div>
+      <LangSwitcher />
+      <p>Current lang: {lang}</p>
+      <hr />
+      <p>Balance: {balance} credits</p>
+      <button onClick={() => dispatch(deposit(10))}>Deposit 10 credits</button>
+      <button onClick={() => dispatch(withdraw(5))}>Withdraw 5 credits</button>
+    </div>
   );
 };
